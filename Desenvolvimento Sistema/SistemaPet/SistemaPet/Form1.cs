ece0ca8f851db01901868ada9e0b1412a21f1306
2B Modular Info <2binfo@basilides.local>
2018-10-18 20:35:53 -0300

    Inicio sistema, Criação da tela Splash #11

---
+++ Desenvolvimento Sistema/SistemaPet/SistemaPet/Form1.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SistemaPet
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            pgbCarregamentoSplash.Style = ProgressBarStyle.Continuous;
+            pgbCarregamentoSplash.ForeColor = Color.FromArgb(0, 179, 161, 143);
+        }
+
+        private void pgbCarregamentoSplash_Click(object sender, EventArgs e)
+        {
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            pgbCarregamentoSplash.Value += 2;
+            if(pgbCarregamentoSplash.Value == 100)
+            {
+                timer1.Enabled = false;
+            }
+        } 
+    }
+}
