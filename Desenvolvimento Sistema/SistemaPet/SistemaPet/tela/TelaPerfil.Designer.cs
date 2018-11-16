c02e0c11609c09331eb5bcc3a7f8cd2364d5913b
Rafael Rios Teixeira <rafaelmowgfx@gmail.com>
2018-11-16 12:07:50 -0200

    Adição TelaPerfil.cs

---
+++ Desenvolvimento Sistema/SistemaPet/SistemaPet/tela/TelaPerfil.Designer.cs
@@ -0,0 +1,166 @@
+﻿namespace SistemaPet.tela
+{
+    partial class TelaPerfil
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.ptbImagemPerfil = new System.Windows.Forms.PictureBox();
+            this.lblNomePerfil = new System.Windows.Forms.Label();
+            this.lblTelefonePerfil = new System.Windows.Forms.Label();
+            this.lblEmailPerfil = new System.Windows.Forms.Label();
+            this.lblRuaPerfil = new System.Windows.Forms.Label();
+            this.lblCidadePerfil = new System.Windows.Forms.Label();
+            this.lblEstadoPerfil = new System.Windows.Forms.Label();
+            this.lblBairroPerfil = new System.Windows.Forms.Label();
+            this.lblNumCasaPerfil = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.ptbImagemPerfil)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // ptbImagemPerfil
+            // 
+            this.ptbImagemPerfil.Location = new System.Drawing.Point(32, 32);
+            this.ptbImagemPerfil.Name = "ptbImagemPerfil";
+            this.ptbImagemPerfil.Size = new System.Drawing.Size(172, 187);
+            this.ptbImagemPerfil.TabIndex = 0;
+            this.ptbImagemPerfil.TabStop = false;
+            // 
+            // lblNomePerfil
+            // 
+            this.lblNomePerfil.AutoSize = true;
+            this.lblNomePerfil.Font = new System.Drawing.Font("Arial Narrow", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblNomePerfil.Location = new System.Drawing.Point(220, 48);
+            this.lblNomePerfil.Name = "lblNomePerfil";
+            this.lblNomePerfil.Size = new System.Drawing.Size(91, 24);
+            this.lblNomePerfil.TabIndex = 1;
+            this.lblNomePerfil.Text = "##Nome##";
+            // 
+            // lblTelefonePerfil
+            // 
+            this.lblTelefonePerfil.AutoSize = true;
+            this.lblTelefonePerfil.Font = new System.Drawing.Font("Arial Narrow", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblTelefonePerfil.Location = new System.Drawing.Point(220, 114);
+            this.lblTelefonePerfil.Name = "lblTelefonePerfil";
+            this.lblTelefonePerfil.Size = new System.Drawing.Size(109, 24);
+            this.lblTelefonePerfil.TabIndex = 2;
+            this.lblTelefonePerfil.Text = "##Telefone##";
+            // 
+            // lblEmailPerfil
+            // 
+            this.lblEmailPerfil.AutoSize = true;
+            this.lblEmailPerfil.Font = new System.Drawing.Font("Arial Narrow", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblEmailPerfil.Location = new System.Drawing.Point(220, 81);
+            this.lblEmailPerfil.Name = "lblEmailPerfil";
+            this.lblEmailPerfil.Size = new System.Drawing.Size(87, 24);
+            this.lblEmailPerfil.TabIndex = 3;
+            this.lblEmailPerfil.Text = "##Email##";
+            // 
+            // lblRuaPerfil
+            // 
+            this.lblRuaPerfil.AutoSize = true;
+            this.lblRuaPerfil.Font = new System.Drawing.Font("Arial Narrow", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblRuaPerfil.Location = new System.Drawing.Point(38, 299);
+            this.lblRuaPerfil.Name = "lblRuaPerfil";
+            this.lblRuaPerfil.Size = new System.Drawing.Size(76, 24);
+            this.lblRuaPerfil.TabIndex = 4;
+            this.lblRuaPerfil.Text = "##Rua##";
+            // 
+            // lblCidadePerfil
+            // 
+            this.lblCidadePerfil.AutoSize = true;
+            this.lblCidadePerfil.Font = new System.Drawing.Font("Arial Narrow", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblCidadePerfil.Location = new System.Drawing.Point(141, 231);
+            this.lblCidadePerfil.Name = "lblCidadePerfil";
+            this.lblCidadePerfil.Size = new System.Drawing.Size(97, 24);
+            this.lblCidadePerfil.TabIndex = 5;
+            this.lblCidadePerfil.Text = "##Cidade##";
+            // 
+            // lblEstadoPerfil
+            // 
+            this.lblEstadoPerfil.AutoSize = true;
+            this.lblEstadoPerfil.Font = new System.Drawing.Font("Arial Narrow", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblEstadoPerfil.Location = new System.Drawing.Point(38, 231);
+            this.lblEstadoPerfil.Name = "lblEstadoPerfil";
+            this.lblEstadoPerfil.Size = new System.Drawing.Size(97, 24);
+            this.lblEstadoPerfil.TabIndex = 6;
+            this.lblEstadoPerfil.Text = "##Estado##";
+            // 
+            // lblBairroPerfil
+            // 
+            this.lblBairroPerfil.AutoSize = true;
+            this.lblBairroPerfil.Font = new System.Drawing.Font("Arial Narrow", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblBairroPerfil.Location = new System.Drawing.Point(38, 265);
+            this.lblBairroPerfil.Name = "lblBairroPerfil";
+            this.lblBairroPerfil.Size = new System.Drawing.Size(88, 24);
+            this.lblBairroPerfil.TabIndex = 7;
+            this.lblBairroPerfil.Text = "##Bairro##";
+            // 
+            // lblNumCasaPerfil
+            // 
+            this.lblNumCasaPerfil.AutoSize = true;
+            this.lblNumCasaPerfil.Font = new System.Drawing.Font("Arial Narrow", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblNumCasaPerfil.Location = new System.Drawing.Point(38, 333);
+            this.lblNumCasaPerfil.Name = "lblNumCasaPerfil";
+            this.lblNumCasaPerfil.Size = new System.Drawing.Size(120, 24);
+            this.lblNumCasaPerfil.TabIndex = 8;
+            this.lblNumCasaPerfil.Text = "##NumCasa##";
+            // 
+            // TelaPerfil
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(857, 500);
+            this.Controls.Add(this.lblNumCasaPerfil);
+            this.Controls.Add(this.lblBairroPerfil);
+            this.Controls.Add(this.lblEstadoPerfil);
+            this.Controls.Add(this.lblCidadePerfil);
+            this.Controls.Add(this.lblRuaPerfil);
+            this.Controls.Add(this.lblEmailPerfil);
+            this.Controls.Add(this.lblTelefonePerfil);
+            this.Controls.Add(this.lblNomePerfil);
+            this.Controls.Add(this.ptbImagemPerfil);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "TelaPerfil";
+            this.Text = "TelaPerfil";
+            ((System.ComponentModel.ISupportInitialize)(this.ptbImagemPerfil)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox ptbImagemPerfil;
+        private System.Windows.Forms.Label lblNomePerfil;
+        private System.Windows.Forms.Label lblTelefonePerfil;
+        private System.Windows.Forms.Label lblEmailPerfil;
+        private System.Windows.Forms.Label lblRuaPerfil;
+        private System.Windows.Forms.Label lblCidadePerfil;
+        private System.Windows.Forms.Label lblEstadoPerfil;
+        private System.Windows.Forms.Label lblBairroPerfil;
+        private System.Windows.Forms.Label lblNumCasaPerfil;
+    }
+}
